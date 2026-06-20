55673b85b0c7e1435f62be75d0796ce6cc1ac67d
moloxe <>
2026-06-20 17:23:11 -0500

    fix: set default local development URL for models-nb service

--- src/stores/models-nb.ts
+++ src/stores/models-nb.ts
@@ -2,7 +2,7 @@ import { useStore } from "@nanostores/react";
 import { atom } from "nanostores";
 import ModelsNBService from "../pages/_services/models-nb";
 
-const baseUrl = atom("");
+const baseUrl = atom("http://127.0.0.1:5001");
 
 export const getBaseUrl = () => {
   return baseUrl.get();
